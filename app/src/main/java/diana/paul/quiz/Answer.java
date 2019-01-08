c9d498ec8408b2dbfc58f9306863017b5e2e0ae0
Prinquipe <45571157+Prinquipe@users.noreply.github.com>
2019-01-08 22:20:55 +0200

    Back to normal

--- app/src/main/java/diana/paul/quiz/Answer.java
+++ app/src/main/java/diana/paul/quiz/Answer.java
@@ -1,4 +1,42 @@
 package diana.paul.quiz;
 
 public class Answer {
+    private int index;
+    private String answer;
+    private String category;
+
+    private static int _count=0;
+
+    public Answer(){
+        index=_count;
+        _count++;
+    }
+
+    public void setCategory(String s){
+        category=s;
+    }
+
+
+    public void setAnswer(String text){
+        answer=text;
+    }
+
+    public String getAnswer(){
+        return answer;
+    }
+
+    @Override
+    public boolean equals( Object obj){
+        if(obj instanceof Answer){
+            if(index==((Answer)obj).index)
+                return true;
+        }
+        return false;
+    }
+
+    public boolean isOfCategory(String c){
+        if(c.equals(category))
+            return true;
+        return false;
+    }
 }
--- app/src/main/java/diana/paul/quiz/library/AnswerLibrary.java
+++ app/src/main/java/diana/paul/quiz/library/AnswerLibrary.java
@@ -1,4 +1,41 @@
+
 package diana.paul.quiz.library;
+import diana.paul.quiz.Answer;
+import diana.paul.quiz.Question;
+import java.util.ArrayList;
+import java.util.Map;
 
 public class AnswerLibrary {
+
+    private ArrayList<Answer> mAnswers;
+    private ArrayList<Question> questions;
+
+    public AnswerLibrary(ArrayList<Answer> ans, ArrayList<Question> que){
+        questions=que;
+        mAnswers=ans;
+    }
+
+    public ArrayList<ArrayList<Answer>> getAnswers(){
+        ArrayList<Answer> ansPool=this.sortAnswers();
+        ArrayList<Answer> shuffle= new ArrayList<>();
+
+        <ArrayList<Answer>> ansMap= ArrayList<ArrayList>();
+
+        for(Answer a: ansPool){
+            for(Question q: questions){
+
+            }
+        }
+    }
+
+    private ArrayList<Answer> sortAnswers(){
+        ArrayList<Answer> sortedAnswer=new ArrayList<>();
+        for(Answer a :mAnswers) {
+            if (a.isOfCategory(questions.get(0).getCategory())) {
+                sortedAnswer.add(a);
+            }
+        }
+        return sortedAnswer;
+    }
 }
+
